ebe1deac06ddc147c3bc642664f9239c6c487732
Rafael B. Salvario <rsalvario3@gmail.com>
2024-03-22 11:07:58 -0300

    Initial commit

---
+++ js/segregacao.js
@@ -0,0 +1,11 @@
+
+console.log('Olá, mundo! Introdução à JS');
+console.log(`Data hora: ${new Date()}`);
+
+function mensagem() {
+    alert('Chamamos um modal');
+}
+
+function mensagem2() {
+    alert('Mensagem 2');
+}
